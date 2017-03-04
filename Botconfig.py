d2ade462f339446cd8edbe5ae1f1c4dcb9fc4e65
CTXz <ctx.xda@gmail.com>
2017-03-04 19:46:25 +0100

    Configs: Fix comments
    The parser would proceed to pick up updates. This was caused because the '#'
    could never be reached in first place.
    
    This has been fixed by adding a comment exception to the parser algorythm
    
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- Botconfig.py
+++ Botconfig.py
@@ -30,58 +30,62 @@ class botcfg:
 	bootmsg = None
 	bootmsgChats = []
 	methods = []
-	methodPackages = [] 
+	methodPackages = []
 
 	def __init__(self, configPath) :
 		self.path = configPath
 		with open(configPath, 'r') as config :
-			configBuffer = config.read() 
-	
+			configBuffer = config.read()
+
 			i      = 0 # Char iterator
 
 			while i < len(configBuffer) and i != -1 :
-			
-				# Check if comment
-				if configBuffer[i] == '#' :
-					i = configBuffer.find('\n', i)
-				else :
-					toggle = 0 # Toggles between abc/non-abc chars and varname start/end elements
-					varnamePosition = [None] * 2 
 
-					# I'm having a hard time explaining this part to others
-					# TODO: Replace with for
-					while True :
-						while i <  len(configBuffer) and configBuffer[i] != '=' and validVarChar(configBuffer[i]) == toggle : i += 1
-						if    i >= len(configBuffer) or configBuffer[i] == '=' : break
+				toggle = 0 # Toggles between abc/non-abc chars and varname start/end elements
+				varnamePosition = [None] * 2
 
-						varnamePosition[toggle] = i 
-						toggle = not toggle
+				# I'm having a hard time explaining this part to others
+				# TODO: Replace with for
+				while True :
 
-					# Get Var name
-					_var = configBuffer[varnamePosition[0]:varnamePosition[1]]
-					_def = configBuffer[i + 1:configBuffer.find(';', i)]
+					while i < len(configBuffer) and configBuffer[i] != '#' and configBuffer[i] != '=' and validVarChar(configBuffer[i]) == toggle :
+						i += 1
 
-					if _var == "BOT_TOKEN" :
-						self.token = _def.strip()
+					if i >= len(configBuffer) or configBuffer[i] == '=' :
+						break
 
-					elif _var == "BOOT_MSG" :
-						try:
-							self.bootmsg = re.findall(r'"([^"]*)"', _def)[0]
-						except IndexError:
-							raise Exception(os.path.basename(self.path) + ": Quotation marks missing on BOOT_MSG definition")
+					if configBuffer[i] == '#' :
+						i = configBuffer.find("\n", i)
 
-					elif _var == "BOOT_MSG_CHATS" :
-						for chat_id in _def.split() :
-							self.bootmsgChats.append(chat_id)
 
-					elif _var == "SUPPORTED_METHODS" :
-						for method in _def.split() :
-							self.methods.append(method)
+					varnamePosition[toggle] = i
+					toggle = not toggle
 
-					i = configBuffer.find(';', i)
+				# Get Var name
+				_var = configBuffer[varnamePosition[0]:varnamePosition[1]]
+				_def = configBuffer[i + 1:configBuffer.find(';', i)]
+
+				if _var == "BOT_TOKEN" :
+					self.token = _def.strip()
+
+				elif _var == "BOOT_MSG" :
+					try:
+						self.bootmsg = re.findall(r'"([^"]*)"', _def)[0]
+					except IndexError:
+						raise Exception(os.path.basename(self.path) + ": Quotation marks missing on BOOT_MSG definition")
+
+				elif _var == "BOOT_MSG_CHATS" :
+					for chat_id in _def.split() :
+						self.bootmsgChats.append(chat_id)
+
+				elif _var == "SUPPORTED_METHODS" :
+					for method in _def.split() :
+						self.methods.append(method)
+
+				i = configBuffer.find(';', i)
 
 	def includesMethod(self, method_name) :
 		return method_name in self.methods
-			
+
 	def exportBot(self) :
 		return telegram.Bot(self.token)
