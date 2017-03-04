18406ac53b159fc2d32c61a7bf7ea126e778dfc4
CTXz <ctx.xda@gmail.com>
2017-03-04 23:08:02 +0100

    Botconfig: Drop method packages
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- Botconfig.py
+++ Botconfig.py
@@ -1,4 +1,3 @@
-
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -30,8 +29,8 @@ class botcfg:
 	bootmsg = None
 	bootmsgChats = []
 	methods = []
-	methodPackages = []
 
+	# Deprecated and will be replaced with configparser module
 	def __init__(self, configPath) :
 		self.path = configPath
 		with open(configPath, 'r') as config :
